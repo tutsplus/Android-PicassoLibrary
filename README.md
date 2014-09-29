a54e8952eaf34813f8f2cb20f9a75e7e5cbcc934
Bart Jacobs <developer@bartjacobs.com>
2014-09-29 07:00:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Tuts+ Tutorial: Android SDK: Working with Picasso
+
+#### Instructor: Leif Johnson
+
+In this quick tip, we take a brief look at the popular Android image library, Picasso. It's a simple and practical library for handling images in your Android projects.
+
+Source files for the Tuts+ tutorial: [Android SDK: Working with Picasso](http://code.tutsplus.com/tutorials/android-sdk-working-with-picasso--cms-22149)
+
+**Read both tutorial on [Tuts+](https://code.tutsplus.com)**
---
+++ app/src/main/java/com/example/leifjohnson/picasotut/MyActivity.java
@@ -0,0 +1,47 @@
+package com.example.leifjohnson.picasotut;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class MyActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this)
+                .load("https://cms-assets.tutsplus.com/uploads/users/21/posts/19431/featured_image/CodeFeature.jpg")
+                .into(imageView);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
